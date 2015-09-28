fbb74d390a126004b6bd72cc927693518407870e
Dmitry Gogenko <gogenko@ukr.net>
2015-09-28 06:57:17 +0300

    polls project - 2parts

---
+++ mysite/polls/models.py
@@ -0,0 +1,23 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        # return 'The quest is %s and pub_date is %s' % (self.question_text, str(self.pub_date))
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return '%s - %d votes' % (self.choice_text, self.votes)
